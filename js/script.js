9268157195cd3fe008e9edb66b6953bb294b50c1
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-04-26 14:07:52 +0200

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -34,3 +34,14 @@ function formSubmit(e) {
   }
 }
 
+// Hamburger menu functionallity (for smaller screens)
+const btn = document.getElementById('menu-btn') // To target the button for the burger menu
+const menu = document.getElementById('menu')    // To target the burger menu itself
+
+btn.addEventListener('click', navToggle)        // To add an event listener to the button
+
+function navToggle() {                          // The function which will run after clicking the button
+  btn.classList.toggle('open')                  // The method toggle() add / removes the class of open (transforms the three lines of the burger menu into an X)
+  menu.classList.toggle('flex')                 // The method toggle() add / removes the class of flex  (shows the menu)
+  menu.classList.toggle('hidden')               // The method toggle() add / removes the class of hidden  (hides the menu)
+}
